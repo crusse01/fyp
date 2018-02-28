45a26005db093eaeec290d36774ec5cda1f7d255
Christian Russell <crusse01@gmail.com>
2018-02-28 11:57:20 +0000

    Seventh commit

---
+++ email.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp@gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'phishingemaildemo@gmail.com';
+$mail->Password = 'phishingfordata';
+$mail->SetFrom('Facebook');
+$mail->Subject = 'I need to verify your password';
+$mail->Body = 'Please give me your password';
+$mail->AddAddress('victimemaildemo@gmail.com');
+
+$mail->send();
+
+?>
