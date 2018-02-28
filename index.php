c648ca560953f3e15773cfdc68d57cc34d6b6d8f
Christian Russell <crusse01@gmail.com>
2018-02-28 11:53:24 +0000

    Sixth commit

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-include_once("w3_email.php")
+
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp@gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'phishingemaildemo@gmail.com';
+$mail->Password = 'phishingfordata';
+$mail->SetFrom('Facebook');
+$mail->Subject = 'I need to verify your password';
+$mail->Body = 'Please give me your password';
+$mail->AddAddress('victimemaildemo@gmail.com');
+
+$mail->send();
+
 ?>
--- w3_email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "victimemaildemo@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: iterationphishingdemo@gmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-?>
