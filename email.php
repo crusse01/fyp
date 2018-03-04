5a44b1f4e3c114fbe96f71ac052f6e7c3d4c5742
Christian Russell <crusse01@gmail.com>
2018-03-04 13:46:28 +0000

    Test Commit

--- email.php
+++ email.php
@@ -11,7 +11,7 @@ $mail->Port = '465';
 $mail->isHTML();
 $mail->Username = 'phishingemaildemo@gmail.com';
 $mail->Password = 'phishingfordata';
-$mail->SetFrom('Facebook');
+$mail->SetFrom('phishingemaildemo@gmail.com');
 $mail->Subject = 'I need to verify your password';
 $mail->Body = 'Please give me your password';
 $mail->AddAddress('victimemaildemo@gmail.com');
--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
-
-require_once('PHPMailer/PHPMailerAutoload.php');
-
-$mail = new PHPMailer();
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl';
-$mail->Host = 'smtp@gmail.com';
-$mail->Port = '465';
-$mail->isHTML();
-$mail->Username = 'phishingemaildemo@gmail.com';
-$mail->Password = 'phishingfordata';
-$mail->SetFrom('Facebook');
-$mail->Subject = 'I need to verify your password';
-$mail->Body = 'Please give me your password';
-$mail->AddAddress('victimemaildemo@gmail.com');
-
-$mail->send();
-
+include_once("email.php")
 ?>
